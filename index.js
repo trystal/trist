af16c274ce55ccbb393ae03228366cff281b5c6e
Terry O'Neill <terry.oneill@trystal.net>
2016-06-20 14:19:06 -0600

    fix a build issue

--- index.js
+++ index.js
@@ -3,102 +3,69 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.mapEm = mapEm;
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+exports.textToChain = textToChain;
+
+var _immutable = require('immutable');
+
+var _immutable2 = _interopRequireDefault(_immutable);
 
 var _lodash = require('lodash');
 
-var _lodash2 = _interopRequireDefault(_lodash);
+var _trist = require('trist');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function mapEm() {
-  var nodes = arguments.length <= 0 || arguments[0] === undefined ? [] : arguments[0];
+var fnLeaderLen = function fnLeaderLen(leader) {
+  return leader.replace(/\t/g, "    ").length;
+};
 
-  var stack = [];
-  var getPV = function getPV(level) {
-    var popped = null;
-    while (stack.length > 0 && _lodash2.default.last(stack).level >= level) {
-      popped = stack.pop();
-    }return popped;
-  };
-  var getPredLevel = function getPredLevel(PV, index) {
-    if (PV) return PV.level;
-    if (index > 0) return nodes[index - 1].level;
-    return 0;
-  };
-  return nodes.reduce(function (accum, _ref, index) {
-    var level = _ref.level;
+var fnMatchToSplit = function fnMatchToSplit(id, _ref) {
+  var _ref2 = _slicedToArray(_ref, 3);
 
-    var PV = getPV(level);
-    accum.push({
-      PV: PV ? PV.index : null,
-      rlevel: level - getPredLevel(PV, index)
-    });
-    stack.push({ index: index, level: level });
+  var leader = _ref2[1];
+  var trystup = _ref2[2];
+  return { id: id, leader: leader, trystup: trystup };
+};
+var fnSplitOne = function fnSplitOne(str, idFactory, index) {
+  return fnMatchToSplit(idFactory(index), str.match(/^(\s*)(.*$)/));
+};
+
+var fnLevelKeys = function fnLevelKeys(leaders) {
+  return (0, _lodash.uniq)(leaders).sort(function (a, b) {
+    return fnLeaderLen(a) - fnLeaderLen(b);
+  });
+};
+var fnLevels = function fnLevels(splitted, levelKeys) {
+  return splitted.reduce(function (accum, item) {
+    var id = item.id;
+    var leader = item.leader;
+
+    accum[id] = levelKeys.indexOf(leader);
     return accum;
-  }, []);
-  // [PV:nn,rlevel:nn]
+  }, {});
+};
+
+function textToChain(text, fnNextId) {
+  var strs = text.split(/[\r\n]+/).filter(function (str) {
+    return str.length;
+  }); // [str,str,...]
+  var splitted = strs.map(function (str, index) {
+    return fnSplitOne(str, fnNextId, index);
+  }); // [{id,leader,trystup},{id,leader,trystup}...]
+  var levelKeys = fnLevelKeys(splitted.map(function (item) {
+    return item.leader;
+  })); // ["", "  ", "    "]
+  var levels = fnLevels(splitted, levelKeys); // [1,0,...]
+  var payloads = splitted.map(function (_ref3) {
+    var id = _ref3.id;
+    var trystup = _ref3.trystup;
+    return { id: id, trystup: trystup };
+  }); // [{id,trystup},{id,trystup},...]
+  var chain = (0, _trist.chainify)(payloads, levels); // [{id,next,prev,NV,PV,rlevel},{...},...]
+  return (0, _trist.collapseAll)(chain, function (id) {
+    return levels[id];
+  }); // {N1:{id,prev,next,PV,NV,rlevel},N2:{...},...}
 }
-
-// nodes[A1,B1,C3,D3,E2,F2,G1,H1,I0]
-//
-// STACK     []
-// ======================
-// NODE-A1:       level     = 1
-//                PV        = (stack,level) => ---  
-//                predLevel = (PV,0) => 0
-//                rlevel    = (level - predlevel) => 1 - 0 => 1
-//                PVI       = PV == null => null
-// ======================
-// RESULT:   [{PVI:null, rlevel: 1}]
-// STACK:    [{index:0,level:1}]  
-// ======================
-// NODE-B1:       level     = 1
-//                PV        = (stack,level) => {index:0,level:1}
-//                predLevel = (PV,1) => PV.level => 1
-//                rlevel    = (level - predlevel) => 1 - 1 => 0
-//                PVI       = PV => PV.index 0
-// ======================
-// RESULT:   [{PVI:null, rlevel: 1},{PVI:0, rlevel: 0}]
-// STACK:    [{index:1, level:1}]  
-// ======================
-// function getPV(stack, level) {
-//   let popped = null
-//   while(stack.length > 0 && _.last(stack).level >= level) popped = stack.pop()
-//   return popped
-// }
-// ======================
-// NODE-C3:       level     = 3, index = 2
-//                PV        = (stack,level=3) => null
-//                predLevel = (PV,index) => nodes[index-1].level => 1
-//                rlevel    = (level - predlevel) => 3 - 1 => 2
-//                PVI       = PV => null 
-// RESULT:   [{PVI:null, rlevel: 1},{PVI:0, rlevel: 0},{PVI:null, rlevel:2}]
-// STACK:    [{index:1, level:1},{index:2,level:3}]  
-// ======================
-// NODE-D3:       level     = 3, index = 3
-//                PV        = (stack,level=3) => {index:2,level:3}
-//                predLevel = (PV,index) =>   PInodes[index-1].level => 1
-//                rlevel    = (level - predlevel) => 3 - 1 => 2
-//                PVI       = PV => null 
-// RESULT:   [{PVI:null, rlevel: 1},{PVI:0, rlevel: 0},{PVI:null, rlevel:2}]
-// STACK:    [{index:1, level:1}]  
-// ======================
-
-// node A2: PV = {index:0,level:1}   STACK: [{index:1,level:1}]  rlevel = (1 - ({0,1},1)) => 1 - 1 => 0, PVI = 0
-// node A3: Pv =   
-
-// result [
-//   {PVI:null, rlevel: 1}
-//   {PVI:0     rlevel: 0}
-// ]
-
-// --A1
-// --A2
-// ------B1
-// ------B2
-// ----B3
-// --- B4
-// --A3
-// --A4
-// A5
